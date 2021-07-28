9c62bf3edf68afc58906c1903e753f7243b876d6
unknown <isfand.zabirov@gmail.com>
2021-07-28 17:49:00 +0500

    First commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func SumOFElements(values []int)  {
+
+	sumOfValues := 0
+	for _, score := range values {
+		sumOfValues += score
+
+	}
+
+	fmt.Printf("The sum of the elements is = %d \n", sumOfValues)
+
+}
+
+func MeanValue(values []int) (int, float64) {
+
+	sumOfValues := 0
+	for _, score := range values {
+		sumOfValues += score
+
+	}
+
+	meanValue := float64(sumOfValues) / float64(len(values))
+
+	return sumOfValues, meanValue
+
+}
+
+
+
+func main() {
+
+	values := []int{4, 5, 1, 6, 9, 10, 32, 54, 12, 34}
+	//SumOFElements(values)
+	fmt.Println(MeanValue(values))
+
+
+}
---
+++ main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestMeanValue(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		values []int
+		SumOfElementsWant int
+		MeanValueWant float64
+	}{
+		{"When all elements are positive", []int{4, 5, 1, 6, 9, 10, 32, 54, 12, 34}, 167, 16.7},
+		{"When all elements are negative", []int{-4, -5, -1, -6, -9, -10, -32, -54, -12, -34}, -167, -16.7},
+		{"When elements are positive and negative", []int{4, -5, -1, 6, 9, -10, -32, 54, 12, 34}, 71, 7.1},
+	}
+
+	for _, test := range tests {
+		SumOFElements, meanValue := MeanValue(test.values)
+
+		if SumOFElements != test.SumOfElementsWant || meanValue != test.MeanValueWant {
+			t.Errorf("MeanValue test %s got SumofElements %d SumofElementswant %d got meanValue %.2f meanValuewant %.2f", test.name, SumOFElements, test.SumOfElementsWant, meanValue, test.MeanValueWant)
+		}
+	}
+
+}
